57269637e44fbaa2247d20fa225037340e1c8183
Steve Langasek <steve.langasek@canonical.com>
2017-11-01 13:48:26 -0400

    If running under UEFI secure boot, attempt to use linuxefi loader
    Forwarded: no
    Last-Update: 2017-11-01
    
    Patch-Name: linuxefi_non_sb_fallback.patch

--- grub-core/loader/i386/efi/linux.c
+++ grub-core/loader/i386/efi/linux.c
@@ -199,7 +199,7 @@ grub_cmd_linux (grub_command_t cmd __attribute__ ((unused)),
   rc = grub_linuxefi_secure_validate (kernel, filelen);
   if (rc < 0)
     {
-      grub_error (GRUB_ERR_INVALID_COMMAND, N_("%s has invalid signature"),
+      grub_error (GRUB_ERR_ACCESS_DENIED, N_("%s has invalid signature"),
 		  argv[0]);
       goto fail;
     }
